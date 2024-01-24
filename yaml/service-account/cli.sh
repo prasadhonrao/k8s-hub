08148c616dc1a3aed21490397908b08486a7b1c4
prasadhonrao <honrao.prasad@gmail.com>
2024-01-24 09:37:36 +0000

    Add service account examples

--- yaml/service-account/cli.sh
+++ yaml/service-account/cli.sh
@@ -1,7 +1,39 @@
-# Create serviceaccount using imperative format
-kubectl create serviceaccount dashboard-admin
-kubectl describe serviceaccount dashboard-admin
+# -----------------------------Imperative-------------------------------------- #
 
-#  Get secret key
-kubectl get secret
-kubectl describe secret dashboard-admin-token-vzqj
+# Get all service accounts
+kubectl get sa
+
+# Create service account using imperative format
+kubectl create sa dashboard-sa
+
+# Create token for the service account
+kubectl create token dashboard-sa
+
+# TODO : Check how to tag the token to the service account as there are lots of changes in the recent K8s versions
+
+# Get service account details
+kubectl describe sa dashboard-sa
+
+# Delete service account
+kubectl delete sa dashboard-sa
+
+# -----------------------------Declarative-------------------------------------- #
+
+# Create a service account
+kubectl apply -f service-account.yaml
+
+# Get service account details
+kubectl describe sa dashboard-sa
+
+# Create a pod using service account
+kubectl apply -f pod.yaml
+
+# Get pod details
+kubectl describe pod nginx-pod
+
+# Delete pod
+kubectl delete -f pod.yaml
+
+# Delete service account
+kubectl delete -f service-account.yaml
+
